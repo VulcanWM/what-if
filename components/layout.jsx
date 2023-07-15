c54cba43943f6a314f20aa301fd1eeaf0e10efab
VulcanWM <logmedha09@gmail.com>
2023-07-15 15:50:47 +0100

    changed meta tags

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import styles from './layout.module.css';
 
-export const siteTitle = "Next Auth Redirect";
+export const siteTitle = "What If";
 
 export default function Layout({ children }) {
   return (
@@ -10,7 +10,7 @@ export default function Layout({ children }) {
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="demo of Next Auth Redirect"
+          content="a website where you will face hypothetical scenarios (what if), and you can enter your thoughts for each of them"
         />
         <meta
           property="og:image"
@@ -18,7 +18,7 @@ export default function Layout({ children }) {
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta property="og:site_name" content="Next Auth Redirect" />
+        <meta property="og:site_name" content={siteTitle} />
         <meta name="robots" content="index, follow"/>
         <meta property="og:type" content="Website" />
         <title>{siteTitle}</title>
