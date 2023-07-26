8296cd02e4c56e45b2119afe3a2e152f656ed80c
VulcanWM <logmedha09@gmail.com>
2023-07-26 12:48:58 +0100

    checking for empty username and username length

--- pages/api/create_account.js
+++ pages/api/create_account.js
@@ -9,10 +9,18 @@ export default async (req, res) => {
         const email = session.user.email;
         const image = session.user.image;
         const username = req.body.username;
+        username = username.trim()
         const email_exists = await get_user_from_email(email);
         if (email_exists){
             res.redirect("/dashboard");
         } else {
+            if (username == ""){
+              res.redirect(`/create_account?error=Your username cannot be empty!`);
+              return;
+            }
+            if (username.length > 20){
+              res.redirect(`/create_account?error=Your username cannot have more than 20 characters!`)
+            }
             const username_exists = await get_user(username)
             if (username_exists){
                 res.redirect(`/create_account?error=${username} has been taken!`)
